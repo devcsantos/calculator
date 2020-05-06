a8ef0224e9ec78f96c4161b090fa1494f8ab8910
Christian Santos <csantos1517@gmail.com>
2020-05-06 20:38:32 +0800

    skeleton design and functions

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+initializePage();
+
+function initializePage() {
+  initializeEventListeners();
+}
+
+function initializeEventListeners() {
+
+}
+
+
+function doAddition(a, b) {
+  return a + b;
+}
+
+function doSubtraction(a, b) {
+  return a - b;
+}
+
+function doMultiplication(a, b) {
+  return a * b;
+}
+
+function doDivision(a, b) {
+  return a / b;
+}
+
+function doExponential(a, b) {
+  return a ** b;
+}
+
+function getSquareRoot(a) {
+  return a ** (1/2);
+}
