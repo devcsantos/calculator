b928120befb7e99c2f425b29ff68f1cc8a1245e1
Christian Santos <csantos1517@gmail.com>
2020-05-09 16:36:31 +0800

    added event listeners for numbers and operations

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+let storedText = '';
+
 initializePage();
 
 function initializePage() {
@@ -5,9 +7,46 @@ function initializePage() {
 }
 
 function initializeEventListeners() {
+  let inputCalculator = document.getElementById('input-calculation');
+  let outputCalculator = document.getElementById('output-calculation');
+  let numberButtons = document.querySelectorAll('#numbers *');
+  let operatorButtons = document.querySelectorAll('#operations *');
+  let clearButton = document.getElementById('clear-btn');
+  let equalsButton = document.getElementById('equals-btn');
 
+  numberButtons.forEach(number => {
+    number.addEventListener('click', () =>{
+      inputCalculator.value = inputCalculator.value.concat(number.innerText);
+    });
+  });
+
+  operatorButtons.forEach(operator => {
+    operator.addEventListener('click', () => {
+      if(operator.innerText != '=') inputCalculator.value = `${inputCalculator.value}${operator.innerText}`;
+    });
+  });
+
+  clearButton.addEventListener('click', clearInput);
+  equalsButton.addEventListener('click', () => {
+    doOperation(inputCalculator.value);
+  });
 }
 
+function clearInput(clearAll = true) {
+  let inputCalculator = document.getElementById('input-calculation');
+  let outputCalculator = document.getElementById('output-calculation');
+  inputCalculator.value = '';
+  if(clearAll) outputCalculator.value = '';
+}
+
+
+function doOperation(inputString) {
+  let inputArray = inputString.split(new RegExp(/([+-/*^])/gm));
+  console.table(inputArray);
+  while(inputArray.length > 1) {
+    
+  }
+}
 
 function doAddition(a, b) {
   return a + b;
